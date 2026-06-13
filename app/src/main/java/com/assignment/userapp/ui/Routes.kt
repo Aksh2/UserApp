c6348a78f28c9a0737cfd3ef8d69dc3a78acc333
Akshay Kumar H <akshaykumarh94@gmail.com>
2026-06-13 21:46:24 +0530

    Add routes

---
+++ app/src/main/java/com/assignment/userapp/ui/Routes.kt
@@ -0,0 +1,9 @@
+package com.assignment.userapp.ui
+
+
+sealed class Screen(val route: String) {
+    data object UserList : Screen("user_list")
+    data object UserDetail : Screen("user_list/{userId}") {
+        fun createRoute(userId: Int) = "user_detail/$userId"
+    }
+}
