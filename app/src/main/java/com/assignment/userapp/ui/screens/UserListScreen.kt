4544e0ad0897272b3ca3cfc17e5100c6e08d4f1c
Akshay Kumar H <akshaykumarh94@gmail.com>
2026-06-13 21:27:41 +0530

    implement userlistscreen

---
+++ app/src/main/java/com/assignment/userapp/ui/screens/UserListScreen.kt
@@ -0,0 +1,137 @@
+package com.assignment.userapp.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import com.assignment.userapp.data.model.User
+import com.assignment.userapp.data.model.UserUiState
+import com.assignment.userapp.viewmodel.UserViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserListScreen(
+    viewModel: UserViewModel = hiltViewModel(),
+    onUserClick: (User) -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    Scaffold(topBar = {
+        TopAppBar(
+            title = { Text("Users") },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                titleContentColor = MaterialTheme.colorScheme.onPrimary
+            )
+        )
+    }) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+        ) {
+            when (val state = uiState) {
+                is UserUiState.Loading -> {
+                    LoadingView()
+                }
+
+                is UserUiState.Success -> {
+                    UserList(
+                        state.user,
+                        onUserClick
+                    )
+                }
+
+                is UserUiState.Error -> {
+                    ErrorView(
+                        message = state.message,
+                        onRetry = viewModel::retry
+                    )
+
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun UserList(
+    user: List<User>,
+    onUserClick: (User) -> Unit
+) {
+    LazyColumn(
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(
+            items = user,
+            key = { it.id }) { userItem ->
+            UserCard(user = userItem)
+        }
+    }
+}
+
+@Composable
+fun UserCard(user: User) {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(user.photo)
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "${user.name} Photo",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .size(56.dp)
+                    .clip(CircleShape)
+            )
+            Text(
+                text = user.name,
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
