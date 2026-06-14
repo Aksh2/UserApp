14dd4749b981cbcc91c5886778e79a3e9a4a1241
Akshay Kumar H <akshaykumarh94@gmail.com>
2026-06-14 11:34:14 +0530

    Add error message

--- app/src/main/java/com/assignment/userapp/data/model/ErrorMessage.kt
+++ app/src/main/java/com/assignment/userapp/data/model/ErrorMessage.kt
@@ -5,4 +5,5 @@ object ErrorMessages{
     const val ERROR_NO_INTERNET = "No internet connection"
     const val ERROR_UNKNOWN = "Something went wrong please try again later"
     const val ERROR_HTTP = "Something went wrong please try again later"
+    const val ERROR_USER_NOT_FOUND = "User Not Found"
 }
