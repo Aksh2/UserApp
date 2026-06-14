6af873d88506abbae2f14e7fcc760095f3f66cbd
Akshay Kumar H <akshaykumarh94@gmail.com>
2026-06-14 11:30:56 +0530

    Add Constants file

---
+++ app/src/main/java/com/assignment/userapp/Constants.kt
@@ -0,0 +1,15 @@
+package com.assignment.userapp
+
+object Constants {
+    const val UNKNOWN = "Unknown"
+    const val NA = "NA"
+    const val NAME = "Name"
+    const val USERNAME = "UserName"
+    const val EMAIL = "Email"
+    const val PHONE = "Phone"
+    const val ADDRESS = "Address"
+
+    const val ZIP = "Zip"
+    const val STATE = "State"
+    const val COUNTRY = "Country"
+}
