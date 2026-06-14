aaab026eea3348621717a01752bec296ed854463
Akshay Kumar H <akshaykumarh94@gmail.com>
2026-06-14 11:32:30 +0530

    fix routes

--- app/src/main/java/com/assignment/userapp/ui/Routes.kt
+++ app/src/main/java/com/assignment/userapp/ui/Routes.kt
@@ -3,7 +3,7 @@ package com.assignment.userapp.ui
 
 sealed class Screen(val route: String) {
     data object UserList : Screen("user_list")
-    data object UserDetail : Screen("user_list/{userId}") {
+    data object UserDetail : Screen("user_detail/{userId}") {
         fun createRoute(userId: Int) = "user_detail/$userId"
     }
 }
